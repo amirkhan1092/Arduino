45515fd5edcdf5612bed0831334a96bc19b129d5
Amir khan <amirkhan1092@gmail.com>
2019-03-15 00:46:22 +0530

    Add files via upload
    keypad switch matrix

---
+++ keypad4x4_example.ino
@@ -0,0 +1,37 @@
+
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {9, 8, 7,6}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);
+}
+  
+void loop(){
+  char key = keypad.getKey();
+  
+  if(key)
+  {
+    Serial.println(key);
+//  delay(100);
+   if(key=='1')
+   digitalWrite(11,!digitalRead(11));
+   if(key=='2')
+   digitalWrite(12,!digitalRead(12));
+   
+  }
+  
+}
