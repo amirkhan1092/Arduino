ea1b56cfda84fe6bde2893cbad186384681d6667
Amir khan <amirkhan1092@gmail.com>
2019-03-15 00:44:37 +0530

    Add files via upload
    LDR with LED ON/OFF

---
+++ ldr_example.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(A0,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int ldr=analogRead(A0);
+Serial.println("LDR value "+String(ldr));
+delay(1000);
+}
